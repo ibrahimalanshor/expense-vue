3312ffc2b06280917a33a7541199d9daf9816be8
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-08-27 12:28:33 +0700

    feat: navbar app

--- src/components/layouts/layout-app.vue
+++ src/components/layouts/layout-app.vue
@@ -1,38 +1,17 @@
 <script>
+import PartialNavbarMenu from './partials/layout-app/partial-navbar-menu.vue';
+
 export default {
   name: 'LayoutApp',
+  components: { PartialNavbarMenu },
 };
 </script>
 
 <template>
-  <nav class="bg-white shadow">
+  <nav class="bg-white border-b border-gray-300">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 justify-between">
-        <div class="flex">
-          <div class="hidden sm:flex sm:space-x-8">
-            <!-- Current: "border-sky-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-            <a
-              href="#"
-              class="inline-flex items-center border-b-2 border-sky-500 px-1 pt-1 text-sm font-medium text-gray-900"
-              >Dashboard</a
-            >
-            <a
-              href="#"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >Team</a
-            >
-            <a
-              href="#"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >Projects</a
-            >
-            <a
-              href="#"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >Calendar</a
-            >
-          </div>
-        </div>
+        <partial-navbar-menu />
         <div class="hidden sm:ml-6 sm:flex sm:items-center">
           <button
             type="button"
---
+++ src/components/layouts/partials/layout-app/partial-navbar-menu.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { translate } from 'src/helpers/translate';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const menus = [
+  {
+    id: 'home',
+    name: translate('navbar.menus.home'),
+    to: { name: 'home' },
+  },
+  {
+    id: 'accounts',
+    name: translate('navbar.menus.accounts'),
+    to: { name: 'accounts.index' },
+  },
+];
+
+function checkIsActive(menu) {
+  return route.matched.some((route) => route.name === menu.id);
+}
+</script>
+
+<template>
+  <div class="flex">
+    <div class="hidden sm:flex sm:space-x-8">
+      <router-link
+        v-for="menu in menus"
+        :key="menu.id"
+        :to="menu.to"
+        :class="[
+          'inline-flex items-center px-1 pt-1 text-sm font-medium',
+          checkIsActive(menu)
+            ? 'text-gray-900'
+            : 'border-transparent text-gray-500 hover:text-gray-700',
+        ]"
+        >{{ menu.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
