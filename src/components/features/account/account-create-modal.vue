e9dbe19062ee973ea6f8a02e2f325f2d1f4d3d81
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-08-27 13:29:16 +0700

    feat: create account modal

---
+++ src/components/features/account/account-create-modal.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BaseModal from 'src/components/base/base-modal.vue';
+import BaseInput from 'src/components/base/base-input.vue';
+import BaseButton from 'src/components/base/base-button.vue';
+import { translate } from 'src/helpers/translate';
+</script>
+
+<template>
+  <base-modal
+    :title="translate('app.accounts.actions.create')"
+    with-header
+    with-footer
+  >
+    <div class="space-y-6">
+      <base-input
+        :label="translate('app.accounts.placeholder.name')"
+        :placeholder="translate('app.accounts.placeholder.name')"
+        with-label
+      />
+      <base-input
+        type="number"
+        :label="translate('app.accounts.placeholder.initial-balance')"
+        :placeholder="translate('app.accounts.placeholder.initial-balance')"
+        with-label
+      />
+    </div>
+
+    <template #footer>
+      <div class="flex items-center justify-end gap-x-2">
+        <base-button color="sky">{{ translate('common.save') }}</base-button>
+        <base-button>{{ translate('common.cancel') }}</base-button>
+      </div>
+    </template>
+  </base-modal>
+</template>
--- src/pages/accounts/account-index-page.vue
+++ src/pages/accounts/account-index-page.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BaseCard from 'src/components/base/base-card.vue';
 import BaseButton from 'src/components/base/base-button.vue';
+import AccountCreateModal from 'src/components/features/account/account-create-modal.vue';
 import { translate } from 'src/helpers/translate';
 </script>
 
@@ -12,4 +13,5 @@ import { translate } from 'src/helpers/translate';
       }}</base-button>
     </template>
   </base-card>
+  <account-create-modal />
 </template>
